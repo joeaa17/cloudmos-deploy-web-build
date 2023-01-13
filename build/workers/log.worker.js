bda2639a873eacf020fcd144cfb2ed9f42a58c56
joeaa17 <joeaa17@gmail.com>
2023-01-13 22:00:54 +0200

    first commit

---
+++ build/workers/log.worker.js
@@ -0,0 +1,65 @@
+const providerProxy = require("../../src/providerProxy");
+const fs = require("fs");
+const syncFs = require("fs");
+const winston = require("winston");
+const helpers = require("../../src/helpers");
+
+let socket;
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.simple(),
+  transports: [new winston.transports.File({ filename: "electron.export.log" })]
+});
+
+process.on("message", async (value) => {
+  logger.info("Exporting logs");
+
+  if (value === "cleanup") {
+    logger.info("Cleanup");
+    socket?.close();
+    socket = null;
+    process.exit(0);
+  }
+
+  const { appPath, url, certPem, prvPem, fileName } = value;
+  const dir = `${appPath}/cloudmos`;
+  const filePath = `${dir}/${fileName}.txt`;
+  let isFinished = false;
+  let lastMessageTimestamp;
+
+  try {
+    if (!syncFs.existsSync(dir)) {
+      syncFs.mkdirSync(dir);
+    }
+
+    await fs.writeFile(filePath, "", {});
+
+    socket = providerProxy.openWebSocket(url, certPem, prvPem, (message) => {
+      let parsedLog = JSON.parse(message);
+      parsedLog.service = parsedLog.name.split("-")[0];
+      parsedLog.message = parsedLog.service + ": " + parsedLog.message;
+
+      syncFs.appendFileSync(filePath, `[${parsedLog.service}]: ${parsedLog.message}\n`);
+
+      lastMessageTimestamp = Date.now();
+    });
+  } catch (error) {
+    logger.error("An error has occured", error);
+  }
+
+  while (!isFinished) {
+    await helpers.sleep(1000);
+
+    const elapsed = Date.now() - lastMessageTimestamp;
+
+    if (Math.floor(elapsed / 1000) > 3) {
+      isFinished = true;
+      socket.close();
+    }
+  }
+
+  logger.info("Finished exporting");
+
+  process.send(filePath);
+});
